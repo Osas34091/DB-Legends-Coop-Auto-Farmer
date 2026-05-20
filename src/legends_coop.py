8e33be191f1c3788cf2747134578ebedd0fd57b3
Osas <70295618+Osas34091@users.noreply.github.com>
2026-05-20 03:12:42 -0600

    Update legends_coop.py

--- src/legends_coop.py
+++ src/legends_coop.py
@@ -1,3 +1,9 @@
+# ==========================================================
+# Proyecto: DB Legends Co-Op Auto Farmer
+# Autor: Osas34091
+# Repositorio: https://github.com/Osas34091/DB-Legends-Coop-Auto-Farmer
+# ==========================================================
+
 import os
 import time
 import random

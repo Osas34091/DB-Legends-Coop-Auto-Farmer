7d669c0867cde87f958b38eadcd8b157d89c2939
Osas <70295618+Osas34091@users.noreply.github.com>
2026-05-20 03:11:26 -0600

    Add input prompt on critical error in main.py
    Added input prompt for closing the window after an error.

--- main.py
+++ main.py
@@ -11,21 +11,17 @@ def print_banner():
            Versión 1.0 | GitHub: Osas34091
     =============================================
     """)
- 
 
 if __name__ == "__main__":
     print_banner()
     
-    if "farm" in sys.argv:
-        debug_active = "--debug" in sys.argv
-        
-        try:
-            device = adb.device()
-            bot = LegendsCoopBot(device, debug_mode=debug_active)
-            bot.start_farming_loop()
-        except Exception as e:
-            print(f"[-] Error crítico al iniciar: {e}")
-            print("[!] Verifica que tu celular esté conectado con Depuración USB activa.")
-    else:
-        print("Uso correcto: python main.py farm")
-        print("Argumentos extra: --debug (muestra logs en consola)")
+    debug_active = "--debug" in sys.argv
+    
+    try:
+        device = adb.device()
+        bot = LegendsCoopBot(device, debug_mode=debug_active)
+        bot.start_farming_loop()
+    except Exception as e:
+        print(f"[-] Error crítico al iniciar: {e}")
+        print("[!] Verifica que tu celular esté conectado con Depuración USB activa.")
+        input("\nPresiona Enter para cerrar esta ventana...")
